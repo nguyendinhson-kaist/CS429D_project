a6d594e20eeed67196024f50b19882a64f9dbb98
Nguyen Dinh Son <nguyendinhson@kaist.ac.kr>
2024-11-29 21:00:04 +0900

    minor fix

--- sampling.py
+++ sampling.py
@@ -2,7 +2,7 @@ import argparse
 
 import numpy as np
 import torch
-from CS492D_project.data.dataset import tensor_to_pil_image
+from .data.dataset import tensor_to_pil_image
 from model import DiffusionModule
 from scheduler import DDPMScheduler
 from pathlib import Path
